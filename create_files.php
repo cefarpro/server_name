be1bee17b0a367704fc2be354d32c401c18e6e16
Pavel Startcev <cefar@mail.ru>
2017-08-22 20:22:21 +0500

    rename + info_file

--- create_files.php
+++ create_files.php
@@ -7,9 +7,16 @@
 	require_once( $path . 'class' . DIRECTORY_SEPARATOR . 'Os.class.php' );
 
 	$name = Os :: generateName( $path_libs . 'adjective.txt', $path_libs . 'noun.txt' );
+	$info = Os :: computerInfo( );
+	$out_info = "PROCESSOR_IDENTIFIER\t" . $info[ 'PROCESSOR_IDENTIFIER' ] . "\n";
+	$out_info .= "PROCESSOR_LEVEL\t" . $info[ 'PROCESSOR_LEVEL' ] . "\n";
+	$out_info .= "NUMBER_OF_PROCESSORS\t" . $info[ 'NUMBER_OF_PROCESSORS' ] . "\n";
+
 	$uid = ChangeWords( $name[ 'noun' ], $name[ 'adjective' ] ) . "\n";
 	$out = Color :: green( $uid );
-	$uid = sprintf( '%u', crc32( $out ) );
+	$out_uid = sprintf( '%u', crc32( $out ) );
 
+	
+	file_put_contents( '_info', $out_info );
 	file_put_contents( '_name', $out );
-	file_put_contents( '_uid', $uid );
+	file_put_contents( '_uid', $out_uid );
