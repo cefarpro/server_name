c81a9ff3a3428e3cfa07eb875705ac23f8fe8954
Pavel Startcev <cefar@mail.ru>
2017-08-22 19:46:04 +0500

    add uid for create 2 file

---
+++ uid.php
@@ -0,0 +1,15 @@
+<?php
+
+	$path = realpath( dirname( __FILE__ ) ) . DIRECTORY_SEPARATOR;
+	$path_libs = $path . 'libs' . DIRECTORY_SEPARATOR;
+	require_once( $path_libs . 'changewords.php' );
+	require_once( $path . 'class' . DIRECTORY_SEPARATOR . 'Color.class.php' );
+	require_once( $path . 'class' . DIRECTORY_SEPARATOR . 'Os.class.php' );
+
+	$name = Os :: generateName( $path_libs . 'adjective.txt', $path_libs . 'noun.txt' );
+	$uid = ChangeWords( $name[ 'noun' ], $name[ 'adjective' ] ) . "\n";
+	$out = Color :: green( $uid );
+	$uid = sprintf( '%u', crc32( $out ) );
+
+	file_put_contents( 'name', $out );
+	file_put_contents( 'uid', $uid );
